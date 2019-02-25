98867cf96e01fffb80dd4b09ec08e64e90992bf8
Cliff <cweinstein.tech@gmail.com>
2019-02-25 14:56:14 -0500

    instruction read/writing, EMEDF support

--- SoulsFormats/Formats/EMEVD.cs
+++ SoulsFormats/Formats/EMEVD.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace SoulsFormats.Formats
 {
@@ -10,9 +12,10 @@ namespace SoulsFormats.Formats
     public class EMEVD : SoulsFile<EMEVD>
     {
         public Game Game { get; set; }
-        private bool IsDS1 => Game == Game.DS1;
-
         public List<Event> Events = new List<Event>();
+
+        private bool IsDS1 => Game == Game.DS1;
+        private EMEDF Documentation;
 
         //for managing data when reading from a file
         internal List<LinkedFile> ReadLinkedFiles = new List<LinkedFile>();
@@ -57,7 +60,10 @@ namespace SoulsFormats.Formats
             /* Read different values depending on if the game targets 32-bit or 64-bit
              * and return them as int. Convert them back to proper types on write.
              */
-
+            if (Game == Game.DS1) Documentation = EMEDF.Read("ds1-common.emedf.json");
+            else if (Game == Game.BB) Documentation = EMEDF.Read("bb-common.emedf.json");
+            else if (Game == Game.DS3) Documentation = EMEDF.Read("ds3-common.emedf.json");
+            
             long uintW() => !IsDS1 ? (long) br.ReadUInt64() : br.ReadUInt32();
             long zeroW() => !IsDS1 ? br.AssertInt64(0) : br.AssertInt32(0);
 
@@ -89,21 +95,20 @@ namespace SoulsFormats.Formats
             /* We jump around the file bceause reading the instructions and parameters
              * first makes it easier to process events.
              */
+            br.Position = argOffset;
+            ReadArgData = new List<byte>(br.ReadBytes((int)argLength));
+
+            br.Position = layOffset;
+            for (long i = 0; i < layCount; i++)
+                ReadLayers.Add(new Layer(br, this));
 
             br.Position = insOffset;
             for (long i = 0; i < insCount; i++)
                 ReadInstructions.Add(new Instruction(br, this));
 
-            br.Position = layOffset;
-            for (long i = 0; i < layCount; i++)
-                ReadLayers.Add(new Layer(br, Game));
-
-            br.Position = argOffset;
-            ReadArgData = new List<byte>(br.ReadBytes((int) argLength));
-
             br.Position = prmOffset;
             for (long i = 0; i < prmCount; i++)
-                ReadParameters.Add(new Parameter(br, Game));
+                ReadParameters.Add(new Parameter(br, this));
 
             br.Position = lnkOffset;
             for (long i = 0; i < lnkCount; i++)
@@ -293,57 +298,174 @@ namespace SoulsFormats.Formats
             }
         }
 
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-        /* EVERYTHING BELOW HERE IS UNFINISHED */
-
         public class Instruction
         {
             EMEVD File;
 
             public uint InstructionClass;
             public uint InstructionIndex;
-            public long ArgLength;
-            public long ArgOffset;
-            public long EventLayerOffset;
+            public EMEDF.ArgDoc[] ArgDocs;
+            public Layer EventLayer = null;
+            public dynamic[] Arguments;
 
-            public Instruction(uint insClass, uint insIndex)
+            public Instruction(uint insClass, uint insIndex, params dynamic[] args)
             {
                 InstructionClass = insClass;
                 InstructionIndex = insIndex;
+                ArgDocs = File.Documentation[InstructionClass][InstructionIndex].Arguments;
+                Arguments = new dynamic[ArgDocs.Length];
+                if (args.Length > 0) SetArgs(args);
+            }
+
+            public void SetArgs(params dynamic[] args)
+            {
+                for (int i = 0; i < Arguments.Length && i < args.Length; i++)
+                {
+                    Arguments[i] = args[i];
+                }
             }
 
             public Instruction(BinaryReaderEx br, EMEVD emevd)
             {
+                File = emevd;
 
+                InstructionClass = br.ReadUInt32();
+                InstructionIndex = br.ReadUInt32();
+                ArgDocs = File.Documentation[InstructionClass][InstructionIndex].Arguments;
+
+                int argLength = File.IsDS1 ? (int)br.ReadUInt32() : (int)br.ReadUInt64();
+                int argOffset = (int)br.ReadUInt32();
+                if (!File.IsDS1) br.AssertInt32(0);
+
+                List<byte> argData = File.ReadArgData.GetRange(argOffset, argLength);
+                Arguments = ReadArgs(argData);
+
+                long layerOffset = File.Game == Game.DS3 ? br.ReadInt64() : br.ReadInt32();
+                if (File.Game != Game.DS3) br.AssertInt32(0);
+                if (layerOffset != -1)
+                {
+                    int layerIndex = (int)(layerOffset / File.LayerSize);
+                    EventLayer = File.ReadLayers[layerIndex];
+                }
+            }
+
+            private dynamic[] ReadArgs(List<byte> argData)
+            {
+                List<dynamic> args = new List<dynamic>();
+
+                var br = new BinaryReaderEx(false, argData.ToArray()); 
+                foreach (var argDoc in ArgDocs)
+                {
+                    if (argDoc.Type == 0) {
+                        args.Add(br.ReadByte());
+                    } else if (argDoc.Type == 1)
+                    {
+                        br.Pad(2);
+                        args.Add(br.ReadUInt16());
+                    } else if (argDoc.Type == 2)
+                    {
+                        br.Pad(4);
+                        args.Add(br.ReadUInt32());
+                    } else if (argDoc.Type == 3)
+                    {
+                        args.Add(br.ReadByte());
+                    }
+                    else if (argDoc.Type == 4)
+                    {
+                        br.Pad(2);
+                        args.Add(br.ReadInt16());
+                    } else if (argDoc.Type == 5)
+                    {
+                        br.Pad(4);
+                        args.Add(br.ReadInt32());
+                    } else if (argDoc.Type == 6)
+                    {
+                        br.Pad(4);
+                        args.Add(br.ReadSingle());
+                    } else if (argDoc.Type == 8)
+                    {
+                        br.Pad(4);
+                        args.Add(br.ReadUInt32());
+                    }
+                }
+
+                return args.ToArray();
             }
 
             public void Write(BinaryWriterEx bw)
             {
 
+                bw.WriteUInt32(InstructionClass);
+                bw.WriteUInt32(InstructionIndex);
+
+                for (int i = 0; i < ArgDocs.Length; i++)
+                {
+                    var doc = ArgDocs[i];
+                    var arg = Arguments[i];
+
+                    if (doc.Type == 0)
+                    {
+                        bw.WriteByte(arg);
+                    } else if (doc.Type == 1)
+                    {
+                        bw.Pad(2);
+                        bw.WriteUInt16(arg);
+                    } else if (doc.Type == 2)
+                    {
+                        bw.Pad(4);
+                        bw.WriteUInt32(arg);
+                    } else if (doc.Type == 3)
+                    {
+                        bw.WriteByte(arg);
+                    }
+                    else if (doc.Type == 4)
+                    {
+                        bw.Pad(2);
+                        bw.WriteInt16(arg);
+                    }
+                    else if (doc.Type == 5)
+                    {
+                        bw.Pad(4);
+                        bw.WriteInt32(arg);
+                    }
+                    else if (doc.Type == 6)
+                    {
+                        bw.Pad(4);
+                        bw.WriteSingle(arg);
+                    }
+                    else if (doc.Type == 8)
+                    {
+                        bw.Pad(4);
+                        bw.WriteUInt32(arg);
+                    }
+                }
             }
         }
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+        /* EVERYTHING BELOW HERE IS UNFINISHED */
+
 
         public class Layer
         {
-            Game Game;
+            public EMEVD File;
 
             public uint LayerNum;
 
-            public Layer(BinaryReaderEx br, Game g)
+            public Layer(BinaryReaderEx br, EMEVD emevd)
             {
-                Game = g;
+                File = emevd;
 
                 br.AssertInt32(2);
                 LayerNum = br.ReadUInt32();
 
-                if (Game == Game.DS1)
+                if (File.IsDS1)
                 {
                     br.AssertUInt32(0);
                     br.AssertInt32(-1);
@@ -365,25 +487,25 @@ namespace SoulsFormats.Formats
 
         public class Parameter
         {
-            public Game Game;
+            public EMEVD File;
 
             public long InstructionNumber;
             public long DestinationStartByte;
             public long SourceStartByte;
             public long Length;
 
-            public Parameter (BinaryReaderEx br, Game g)
+            public Parameter (BinaryReaderEx br, EMEVD emevd)
             {
-                Game = g;
+                File = emevd;
 
-                long uintW() => Game != Game.DS1 ? (long) br.ReadUInt64() : br.ReadUInt32();
+                long uintW() => File.IsDS1 ? br.ReadUInt32() : (long) br.ReadUInt64();
 
                 InstructionNumber = uintW();
                 DestinationStartByte = uintW();
                 SourceStartByte = uintW();
                 Length = uintW();
 
-                if (Game == Game.DS1) br.AssertUInt32(0);
+                if (File.IsDS1) br.AssertUInt32(0);
             }
 
             public void Write (BinaryWriterEx bw)
@@ -410,7 +532,104 @@ namespace SoulsFormats.Formats
             }
         }
 
-        #endregion
+        public class EMEDF
+        {
+            public ClassDoc this[uint i] => Classes.Find(c => c.Index == i);
 
+            [JsonProperty(PropertyName = "unknown")]
+            private long UNK;
+
+            [JsonProperty(PropertyName = "main_classes")]
+            private List<ClassDoc> Classes;
+
+            [JsonProperty(PropertyName = "enums")]
+            public EnumDoc[] Enums;
+
+            public static EMEDF Read (string path)
+            {
+                string input = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<EMEDF>(input);
+            }
+
+            public class ClassDoc
+            {
+                [JsonProperty(PropertyName = "name")]
+                public string Name { get; set; }
+
+                [JsonProperty(PropertyName = "index")]
+                public long Index { get; set; }
+
+                [JsonProperty(PropertyName = "instrs")]
+                public List<InstrDoc> Instructions { get; set; }
+
+                public InstrDoc this[uint i] => Instructions.Find(ins => ins.Index == i);
+            }
+
+            public class InstrDoc
+            {
+                [JsonProperty(PropertyName = "name")]
+                public string Name { get; set;  }
+
+                [JsonProperty(PropertyName = "index")]
+                public long Index { get; set; }
+
+                [JsonProperty(PropertyName = "args")]
+                public ArgDoc[] Arguments { get; set; }
+
+                public ArgDoc this[uint i] => Arguments[i];
+            }
+
+            public class ArgDoc
+            {
+                [JsonProperty(PropertyName = "name")]
+                public string Name { get; set; }
+
+                [JsonProperty(PropertyName = "type")]
+                public long Type { get; set; }
+
+                [JsonProperty(PropertyName = "enum_name")]
+                public string EnumName { get; set; }
+
+                [JsonProperty(PropertyName = "default")]
+                public long Default { get; set; }
+
+                [JsonProperty(PropertyName = "min")]
+                public long Min { get; set; }
+
+                [JsonProperty(PropertyName = "max")]
+                public long Max { get; set; }
+
+                [JsonProperty(PropertyName = "increment")]
+                public long Increment { get; set; }
+
+                [JsonProperty(PropertyName = "format_string")]
+                public string FormatString { get; set; }
+
+                [JsonProperty(PropertyName = "unk1")]
+                private long UNK1 { get; set; }
+
+                [JsonProperty(PropertyName = "unk2")]
+                private long UNK2 { get; set; }
+
+                [JsonProperty(PropertyName = "unk3")]
+                private long UNK3 { get; set; }
+
+                [JsonProperty(PropertyName = "unk4")]
+                private long UNK4 { get; set; }
+            }
+
+            public class EnumDoc
+            {
+                [JsonProperty(PropertyName = "name")]
+                public string Name { get; set; }
+
+                [JsonProperty(PropertyName = "values")]
+                public Dictionary<string, string> Values { get; set; }
+            }
+        }
+
+        #endregion
     }
 }
+
+
